113e7c85b955c8821ef2b7c57b777471072be036
Valentina Valverde <vvgreenville@gmail.com>
2024-07-31 16:29:45 -0400

    Random commit!

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './styles/page.module.css';
 export default function Home() {
   return (
     <main>
-      <div>
+      <div className={styles.test}>
         <p>Midway</p>
       </div>
     </main>
