692fe75bf1cfa448f7a62730949ab3a8adca01c8
Valentina Valverde <vvgreenville@gmail.com>
2024-07-31 17:53:40 -0400

    Building out landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
+import React from 'react';
 import styles from './styles/page.module.css';
+import Logo from '../public/icons/midway-logo.png';
 
 export default function Home() {
   return (
     <main>
-      <div>
-        <p>Midway</p>
+      {/* insert image tag here */}
+
+      <div
+        style={{
+          maxWidth: '100%',
+          textAlign: 'right',
+          lineHeight: 1.5,
+          padding: 20,
+
+          position: 'absolute',
+          top: 100,
+          right: 20,
+        }}
+      >
+        <p>
+          Ever wondered what lies between <br />
+          you and your friends? Well, there’s <br />
+          finally a way to figure it out!
+        </p>
+        <button className={styles.primary_button}>Start</button>
       </div>
+
+      <p className={styles.homepage_midway}>Midway</p>
     </main>
   );
 }
